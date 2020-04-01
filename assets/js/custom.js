c4cd88b4f871ef3b3443cea224e248adae0a6ccd
dani17r <danielrangel17md@hotmail.com>
2020-04-01 19:32:36 -0400

    carousel + responsive. listos

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,28 @@
 /*
-  funcionalidad personalizada
+funcionalidad personalizada
 */
+$(function() {
+    $('#autoWidth').lightSlider({
+        item:2,
+        pager:false,
+        enableDrag: false,
+        keyPress:true,
+        // verticalHeight:300,
+        slideMargin:-14,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden')
+        },
+        responsive : [
+            {
+                breakpoint:700,
+                settings: {
+                    item:1,
+                    // verticalHeight:500,
+                    // slideMove:1,
+                    // slideMargin:6,
+                  }
+            },
+        ]
+    })
+
+})
